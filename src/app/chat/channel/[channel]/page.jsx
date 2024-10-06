8f0b402da1fd50fe1b7fbbb0a32467fbfdaffec4
Alper Aktas <aktasalper0708@gmail.com>
2024-10-06 21:14:06 +0300

    developments vdeo call

--- src/app/chat/channel/[channel]/page.jsx
+++ src/app/chat/channel/[channel]/page.jsx
@@ -1,10 +1,18 @@
 'use client';
 
 import Call from "@/components/chat/video-call";
+import { useEffect } from "react";
 
 export default function Page(params) {
+    useEffect(()=>
+    {
+        if(!params.params.channel)
+        {
+            document.body.style.overflow = 'hidden';
+        }
+    },[])
     return (
-        <main className="flex w-full flex-col">
+        <main className="flex w-screen flex-col h-screen overflow-auto fixed z-[999] bg-black top-0 left-0">
             <Call appId={process.env.NEXT_PUBLIC_AGORA_APP_ID ?? '4cc7a85443014303bb31372a087ec038'} channelName={params.params.channel}></Call>
         </main>
     )
--- src/components/chat/video-call.js
+++ src/components/chat/video-call.js
@@ -1,7 +1,9 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import AgoraRTC, {
   AgoraRTCProvider,
+  LocalAudioTrack,
   LocalVideoTrack,
   RemoteUser,
   useJoin,
@@ -12,6 +14,13 @@ import AgoraRTC, {
   useRemoteAudioTracks,
   useRemoteUsers,
 } from "agora-rtc-react";
+import {
+  BsCameraVideo,
+  BsCameraVideoOff,
+  BsMic,
+  BsMicMute,
+} from "react-icons/bs";
+import { MdCallEnd, MdCameraswitch } from "react-icons/md";
 
 async function Call(props) {
   if (!window) {
@@ -24,25 +33,26 @@ async function Call(props) {
   return (
     <AgoraRTCProvider client={client}>
       <Videos channelName={props.channelName} AppID={props.appId} />
-      <div className="fixed z-10 bottom-0 left-0 right-0 flex justify-center pb-4">
-        <a
-          className="px-5 py-3 text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40"
-          href="/"
-        >
-          End Call
-        </a>
-      </div>
     </AgoraRTCProvider>
   );
 }
 
 function Videos(props) {
+
   const { AppID, channelName } = props;
   const { isLoading: isLoadingMic, localMicrophoneTrack } =
     useLocalMicrophoneTrack();
   const { isLoading: isLoadingCam, localCameraTrack } = useLocalCameraTrack();
+  const [availableCameras, setAvailableCameras] = useState([]);
+  const [currentCameraIndex, setCurrentCameraIndex] = useState(0);
   const remoteUsers = useRemoteUsers();
   const { audioTracks } = useRemoteAudioTracks(remoteUsers);
+
+  const [isMuted, setIsMuted] = useState(false);
+  const [isCameraOff, setIsCameraOff] = useState(false);
+  const [isControlsVisible, setIsControlsVisible] = useState(true);
+
+  const inactivityTimeout = useRef(null);
 
   usePublish([localMicrophoneTrack, localCameraTrack]);
   useJoin({
@@ -50,19 +60,85 @@ function Videos(props) {
     channel: channelName,
     token: null,
   });
+  useEffect(() => {
+    AgoraRTC.getCameras().then((cameras) => {
+      setAvailableCameras(cameras);
+    });
+
+    resetInactivityTimer();
+    window.addEventListener("touchstart", resetInactivityTimerOnClick);
+    window.addEventListener("click", resetInactivityTimerOnClick);
+
+    return () => {
+      window.removeEventListener("touchstart", resetInactivityTimerOnClick);
+      window.removeEventListener("click", resetInactivityTimerOnClick);
+
+      if (inactivityTimeout.current) {
+        clearTimeout(inactivityTimeout.current);
+      }
+    };
+  }, []);
+
+  const resetInactivityTimer = () => {
+    if (inactivityTimeout.current) {
+      clearTimeout(inactivityTimeout.current);
+    }
+    setIsControlsVisible(true);
+    inactivityTimeout.current = setTimeout(() => {
+      setIsControlsVisible(false);
+    }, 2000);
+  };
+
+  const resetInactivityTimerOnClick = () => {
+    if (isControlsVisible) {
+      setIsControlsVisible(false);
+    }
+    resetInactivityTimer();
+  };
 
   audioTracks.map((track) => track.play());
   const deviceLoading = isLoadingMic || isLoadingCam;
-  if (deviceLoading)
-    return (
-      <div className="flex flex-col items-center pt-40">Loading devices...</div>
-    );
-  const unit = "minmax(0, 1fr) ";
 
+  if (deviceLoading) {
+    return (
+      <div className="flex flex-coTl items-center pt-40">
+        Loading devices...
+      </div>
+    );
+  }
+  const switchCamera = async () => {
+    if (availableCameras.length > 1) {
+      const nextCameraIndex =
+        (currentCameraIndex + 1) % availableCameras.length;
+      const newCameraDeviceId = availableCameras[nextCameraIndex].deviceId;
+      localCameraTrack.setDevice(newCameraDeviceId);
+
+      setCurrentCameraIndex(nextCameraIndex);
+    }
+  };
+  const handleMute = () => {
+    if (isMuted) {
+      localMicrophoneTrack.setEnabled(true);
+    } else {
+      localMicrophoneTrack.setEnabled(false);
+    }
+    setIsMuted(!isMuted);
+  };
+
+  const handleCameraToggle = () => {
+    if (isCameraOff) {
+      localCameraTrack.setEnabled(true);
+    } else {
+      localCameraTrack.setEnabled(false);
+    }
+    setIsCameraOff(!isCameraOff);
+  };
+
+  const unit = "minmax(0, 1fr) ";
   return (
-    <div className="flex flex-col justify-between w-full h-[80vh] p-1">
+    <div className="flex flex-col justify-between w-full h-screen p-1">
       <div
-        className={`grid  gap-1 flex-1`}
+        className={`grid gap-1 flex-1`}
         style={{
           gridTemplateColumns:
             remoteUsers.length > 9
@@ -74,15 +150,92 @@ function Videos(props) {
               : unit,
         }}
       >
-        <LocalVideoTrack
-          track={localCameraTrack}
-          play={true}
-          className="w-full h-full"
-        />
-        {remoteUsers.map((user) => (
-          <RemoteUser user={user} />
-        ))}
+        {/* Local Video Track */}
+        <div className="relative w-full h-full">
+          {isCameraOff ? (
+            // Kamera kapalıysa ikon göster
+            <div className="flex justify-center items-center w-full h-full bg-black relative">
+              <BsCameraVideoOff
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                size={80}
+                color="white"
+              />
+            </div>
+          ) : (
+            // Kamera açıksa video oynat
+            <LocalVideoTrack
+              track={localCameraTrack}
+              play={true}
+              className="w-full h-full relative"
+            >
+              {isMuted && (
+                <div className="absolute flex justify-center items-center w-full top-0 h-full bg-transparent z-10">
+                  <BsMicMute size={80} color="white" />
+                </div>
+              )}
+            </LocalVideoTrack>
+          )}
+        </div>
+
+        {/* Remote Users Video Tracks */}
+        {remoteUsers.map((remoteUser) => {
+          return (
+            <div key={remoteUser.uid} className="relative w-full h-full">
+              {remoteUser.hasVideo ? (
+                <RemoteUser user={remoteUser} className="w-full h-full">
+                  {!remoteUser.audioTrack && (
+                    <div className="flex justify-center items-center w-full h-full bg-transparent">
+                      <BsMicMute size={80} color="white" />
+                    </div>
+                  )}
+                </RemoteUser>
+              ) : (
+                <div className="flex justify-center items-center w-full h-full bg-black relative">
+                  <BsCameraVideoOff
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                    size={80}
+                    color="white"
+                  />
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
+
+      {isControlsVisible && (
+        <div className="fixed z-10 left-0 right-0 flex justify-center w-fit mx-auto p-4 rounded-2xl bg-gray-900 bottom-5">
+          <button
+            onClick={handleMute}
+            className={` text-base font-medium text-center text-white rounded-lg mx-2`}
+          >
+            {isMuted ? <BsMicMute size={50} /> : <BsMic size={50} />}
+          </button>
+          <button
+            onClick={handleCameraToggle}
+            className={` text-base font-medium text-center text-white rounded-lg mx-2`}
+          >
+            {isCameraOff ? (
+              <BsCameraVideoOff size={50} />
+            ) : (
+              <BsCameraVideo size={50} />
+            )}
+          </button>
+          <button
+            onClick={switchCamera}
+            className={` text-base font-medium text-center text-white rounded-lg mx-2`}
+          >
+            <MdCameraswitch size={50} />
+          </button>
+
+          <a
+            className="text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 mx-2"
+            href="/"
+          >
+            <MdCallEnd size={50} />
+          </a>
+        </div>
+      )}
     </div>
   );
 }
