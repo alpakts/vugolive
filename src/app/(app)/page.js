756d377f952f9d87f8ded88462a0e784b08e9718
Alper Aktas <aktasalper0708@gmail.com>
2024-12-30 00:44:37 +0300

    dev

--- src/app/(app)/page.js
+++ src/app/(app)/page.js
@@ -1,4 +1,5 @@
 import HomeIndex from "@/components/home";
+import Script from "next/script";
 
 export const metadata = {
   title: 'Vugo Live | Canlı Yayınlar',
@@ -12,6 +13,24 @@ export default function Home() {
   return (
     <div>
       <HomeIndex></HomeIndex>
+      <Script
+        id="tawk-to-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/676d83ca49e2fd8dfefe5818/1ig1slka6';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+        }}
+      />
     </div>
   );
+ 
 }
